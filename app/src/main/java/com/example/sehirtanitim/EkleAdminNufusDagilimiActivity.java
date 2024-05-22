64179a136d0b572ff88dc4edc3e83ae56bd00f90
furkanyapan <furkanyapan@hotmail.com>
2024-05-22 23:13:52 +0300

    population rate for user and admin

---
+++ app/src/main/java/com/example/sehirtanitim/EkleAdminNufusDagilimiActivity.java
@@ -0,0 +1,57 @@
+package com.example.sehirtanitim;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EkleAdminNufusDagilimiActivity extends AppCompatActivity {
+    private Button btnEkle, btnAdminMain;
+    private EditText edtIlceAdi, edt2023;
+    private SQLiteDatabase vt;
+    private Veritabani db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ekle_admin_nufus_dagilimi);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        db = new Veritabani(this);
+
+        btnEkle = findViewById(R.id.btnEkle);
+        btnAdminMain = findViewById(R.id.btnAdminMain);
+        edtIlceAdi = findViewById(R.id.edtIlceAdi);
+        edt2023 = findViewById(R.id.edt2023);
+
+        btnEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vt = db.getWritableDatabase();
+                String sorgu = "INSERT INTO Tablo (ilce_name, nufus_2023) VALUES ('" + edtIlceAdi.getText().toString() + "','" + edt2023.getText().toString() + "')";
+                vt.execSQL(sorgu);
+                Toast.makeText(getApplicationContext(),"Kayıt Eklendi",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnAdminMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EkleAdminNufusDagilimiActivity.this, AdminMainActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/sehirtanitim/NufusDagilimiActivity.java
+++ app/src/main/java/com/example/sehirtanitim/NufusDagilimiActivity.java
@@ -1,10 +1,15 @@
 package com.example.sehirtanitim;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class NufusDagilimiActivity extends AppCompatActivity {
     private Button btnMain;
     private ListView listViewNufusDagilimi;
+
+    private ArrayList<String> nufuslar;
+    private ArrayList<Tablo> nufusListesi;
+
+    private Veritabani vt;
+    private ArrayAdapter<String> adt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +41,25 @@ public class NufusDagilimiActivity extends AppCompatActivity {
 
         btnMain = findViewById(R.id.btnMain);
         listViewNufusDagilimi = findViewById(R.id.listViewNufusDagilimi);
+
+        nufuslar = new ArrayList<String>();
+        nufusListesi = new ArrayList<Tablo>();
+        vt = new Veritabani(this);
+        SQLiteDatabase db = vt.getReadableDatabase();
+        String sorgu = "SELECT * FROM Tablo";
+        Cursor c = db.rawQuery(sorgu, null);
+        while (c.moveToNext()){
+            nufusListesi.add(new Tablo(c.getInt(0), c.getString(1), c.getInt(2), c.getInt(3), c.getInt(4), c.getString(5)));;
+        }
+        for (int i=0; i<nufusListesi.size(); i++){
+            String ilceadi = nufusListesi.get(i).getIlce_name();
+            int nufus23 = nufusListesi.get(i).getNufus_2023();
+            int toplamnufus = 1273519;
+            double oran = ((double) nufus23 / toplamnufus) * 100;
+            nufuslar.add(ilceadi + " " + nufus23 + " %" + String.format("%.2f", oran));
+        }
+        adt = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, nufuslar);
+        listViewNufusDagilimi.setAdapter(adt);
 
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
